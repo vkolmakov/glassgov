d4c8080018cd5b7a715289770de2d52b24a445a7
vkolmakov <vkolmakov@hotmail.com>
2017-03-23 10:47:58 -0500

    Add vue-star-rating

--- client/src/components/StarRating.vue
+++ client/src/components/StarRating.vue
@@ -1,9 +1,37 @@
 <template>
-  <h2>{{ rating }} stars</h2>
+  <star-rating
+      :read-only="!isWritable"
+      :rating="rating"
+      :increment="isWritable ? 1.0 : 0.01"
+      :active-color="primaryColor"
+      :inactive-color="tertiaryColor"
+      :border-color="secondaryColor"
+      :star-size="25"></star-rating>
 </template>
 
 <script>
+import StarRating from 'vue-star-rating'
+
 export default {
-  props: ['rating'],
+  props: ['rating', 'isWritable'],
+  components: {
+    StarRating,
+  },
+  methods: {
+    getCssVariable(name) {
+      return window.getComputedStyle(document.body).getPropertyValue(name)
+    },
+  },
+  computed: {
+    primaryColor() {
+      return this.getCssVariable('--primary-color')
+    },
+    secondaryColor() {
+      return this.getCssVariable('--secondary-color')
+    },
+    tertiaryColor() {
+      return this.getCssVariable('--tertiary-color')
+    }
+  },
 }
 </script>
