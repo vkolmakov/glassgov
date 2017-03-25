1f61e684c7c163f8078091e50fa962dc0257709a
vkolmakov <vkolmakov@hotmail.com>
2017-03-24 21:38:39 -0500

    Update list of employees

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,11 @@
 const mockEmployees = [
-  { name: 'John Duh', title: 'Mayor', department: 'City', salary: 200000, rating: 3.2, id: 0, photo: 'https://s-media-cache-ak0.pinimg.com/736x/44/2a/8c/442a8cdde3891bdabb4763d9d208c494.jpg' },
-  { name: 'Jane Doe', title: 'Treasurer', department: 'City', salary: 200000, rating: 4.5, id: 1, photo: 'https://pbs.twimg.com/profile_images/552145045663408129/egywNuyx.png' },
-  { name: 'Chuck Norris', title: 'Badass', department: 'City', salary: 999999, rating: 5.0, id: 2, photo: 'https://cbsdallas.files.wordpress.com/2010/12/walker.jpg?w=640' },
+  { name: 'Philip J. Fry', title: 'Delivery Boy', department: 'Planet Express Crew', salary: 30000, rating: 3.2, id: 0, photo: 'https://pbs.twimg.com/profile_images/806313367052976129/CketiJDK.jpg' },
+  { name: 'Turanga Leela', title: 'Captain', department: 'Planet Express Crew', salary: 50000, rating: 4.5, id: 1, photo: 'https://geekygirlfilmblog.files.wordpress.com/2014/04/turanga_leela_314.png' },
+  { name: 'Bender Bending Rodrigez', title: 'Industrial Robot', department: 'Planet Express Crew', salary: 30000, rating: 5.0, id: 2, photo: 'http://idaconcpts.com/wp-content/uploads/bender-smoking.jpg' },
+  { name: 'Amy Wong', title: 'Intern', department: 'Planet Express Crew', salary: 0, rating: 5.0, id: 3, photo: 'https://upload.wikimedia.org/wikipedia/it/1/12/Amy_wong_-_futurama.png' },
+  { name: 'Hermes Conrad', title: 'Bureaucrat and Accountant', department: 'Planet Express', salary: 50000, rating: 5.0, id: 4, photo: 'http://vignette1.wikia.nocookie.net/futurama/images/9/95/Hermes_Conrad.png/revision/latest?cb=20090809214221&path-prefix=it' },
+  { name: 'Professor Hubert J. Farnsworth', title: 'CEO', department: 'Planet Express', salary: 50000, rating: 4.4, id: 5, photo: 'http://vignette2.wikia.nocookie.net/en.futurama/images/f/f6/Professor-farnsworth.jpg/revision/latest/zoom-crop/width/240/height/240?cb=20120226073833' },
+  { name: 'Doctor John A. Zoidberg', title: 'Staff Doctor', department: 'Planet Express', salary: 5000, rating: 3.0, id: 6, photo: 'https://uproxx.files.wordpress.com/2014/10/zoidberg.jpg?quality=100&w=600' },
 ]
 
 const mockComments = [
@@ -24,4 +28,12 @@ export function getComments(id) {
       resolve(mockComments)
     }, 300)
   })
+}
+
+export function getFeatured() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(mockEmployees.slice(0, 3))
+    })
+  }, 300)
 }
