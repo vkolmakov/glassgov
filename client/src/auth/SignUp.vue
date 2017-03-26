4d9ac153ef062cb8c77f323db725967d12f5a42f
vkolmakov <vkolmakov@hotmail.com>
2017-03-26 11:25:01 -0500

    Add sign up form placeholder

--- client/src/auth/SignUp.vue
+++ client/src/auth/SignUp.vue
@@ -1,22 +1,42 @@
 <template>
-  <div>
-
-    <h1>Sign Up</h1>
-
-    <div>
-      <label>Email</label>
-      <input type="email" />
-    </div>
-
-    <div>
-      <label>Password</label>
-      <input type="password" />
-    </div>
-
-    <div>
-      <label>Repeat Password</label>
-      <input type="password" />
-    </div>
-
-  </div>
+  <form-template
+      :title="form.title"
+      :fields="form.fields"
+      :submitButtonText="form.submitButtonText"
+      :onSubmit="form.onSubmit"></form-template>
 </template>
+
+<script>
+import FormTemplate from '../components/FormTemplate.vue'
+
+export default {
+  components: {
+    FormTemplate,
+  },
+  data() {
+    return {
+      form: {
+        title: 'Sign Up',
+        submitButtonText: 'Sign Up',
+        fields: [{
+          name: 'email',
+          label: 'Email',
+          type: 'email',
+          validate: email => email && email.includes('@') && email.includes('.'),
+        }, {
+          name: 'password',
+          label: 'Password',
+          type: 'password',
+          validate: password => password && password.length > 8,
+        }, {
+          name: 'repeat-password',
+          label: 'Repeat Password',
+          type: 'password',
+          validate: repeatPassword => repeatPassword && repeatPassword.length > 8,
+        }],
+        onSubmit: formData => console.log("submitted!", formData)
+      }
+    }
+  }
+}
+</script>
