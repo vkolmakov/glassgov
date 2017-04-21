1d46493231b8aa3c712f5836f052ac2626816675
vkolmakov <vkolmakov@hotmail.com>
2017-04-21 14:01:22 -0500

    Add axios+fuse to utils

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,6 +1,9 @@
 import * as FolktaleMaybe from 'folktale/data/maybe'
 import R from 'ramda'
+import Fuse from 'fuse.js'
+import _axios from 'axios'
 import router from './router'
+
 
 export const Maybe = FolktaleMaybe
 export const compose = R.compose
@@ -20,3 +23,23 @@ export const take = R.take
 
 export const trace = msg => R.tap(x => console.log(msg, x))
 export const redirectTo = name => router.push({ name })
+
+export const axios = _axios
+
+export const createSearchEngine = (list) => {
+  const options = {
+    shouldSort: false,
+    threshold: 0.5,
+    location: 0,
+    distance: 100,
+    maxPatternLength: 32,
+    minMatchCharLength: 1,
+    keys: [
+      'name',
+      'title',
+      'department',
+    ],
+  }
+
+  return new Fuse(list, options)
+}
