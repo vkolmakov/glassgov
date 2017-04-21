43bb3ba0a5587fef6190e2aab9398fcaf82826da
vkolmakov <vkolmakov@hotmail.com>
2017-04-21 13:56:39 -0500

    Add vis link to homepage

--- client/src/home/Home.vue
+++ client/src/home/Home.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
-    <div class="description">
-      <h1>Learn about Chicago with GlassGov</h1>
-      <p>Feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi. Turpis egestas integer eget aliquet nibh praesent tristique magna sit amet purus gravida quis blandit turpis cursus in hac habitasse! Dictum sit amet justo donec enim diam, vulputate ut pharetra sit amet, aliquam id diam maecenas ultricies mi? Mattis enim ut tellus elementum sagittis vitae et leo duis ut diam?</p>
-      <p>Nunc vel risus commodo viverra maecenas accumsan, lacus vel facilisis. Mauris vitae ultricies leo integer malesuada nunc vel risus commodo viverra maecenas accumsan, lacus vel facilisis volutpat, est velit egestas.</p>
+    <div class="top">
+      <div class="description">
+        <h1>Learn about Chicago with GlassGov</h1>
+        <p>Feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi. Turpis egestas integer eget aliquet nibh praesent tristique magna sit amet purus gravida quis blandit turpis cursus in hac habitasse! Dictum sit amet justo donec enim diam, vulputate ut pharetra sit amet, aliquam id diam maecenas ultricies mi? Mattis enim ut tellus elementum sagittis vitae et leo duis ut diam?</p>
+        <p>Nunc vel risus commodo viverra maecenas accumsan, lacus vel facilisis. Mauris vitae ultricies leo integer malesuada nunc vel risus commodo viverra maecenas accumsan, lacus vel facilisis volutpat, est velit egestas.</p>
+      </div>
     </div>
 
     <div class="featured">
       <h1>Featured Officials</h1>
       <featured-list :maybeFeatured="maybeFeatured"></featured-list>
+    </div>
+
+    <div class="links">
+      <link-wrapper :to="{ name: 'visualize' }"><i class="fa fa-bar-chart" aria-hidden="true"></i>Visualize</link-wrapper>
     </div>
 
   </div>
@@ -18,10 +24,12 @@
 import { mapState } from 'vuex'
 
 import FeaturedList from './FeaturedList.vue'
+import LinkWrapper from '../components/LinkWrapper.vue'
 
 export default {
   components: {
     FeaturedList,
+    LinkWrapper,
   },
 
   computed: mapState({
@@ -31,12 +39,12 @@ export default {
 </script>
 
 <style>
+
 .description {
   border: solid 1px var(--secondary-color);
   background-color: var(--tertiary-color);
   border-radius: 0.3em;
   padding: 1em;
-  margin: 1em 0;
 }
 
 .description > h1 {
@@ -46,5 +54,10 @@ export default {
 
 .featured > h1 {
   text-align: center;
+}
+
+.links {
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
