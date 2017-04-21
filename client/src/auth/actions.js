9d5127e9795acad0bc7bd820774c772085f2e417
vkolmakov <vkolmakov@hotmail.com>
2017-04-21 13:56:17 -0500

    Save token on auth

--- client/src/auth/actions.js
+++ client/src/auth/actions.js
@@ -1,13 +1,24 @@
+import { axios, Maybe } from '../utils'
+
+const TOKEN_ITEM_KEY = 'token'
+
 export function setAuthToken(token) {
   return new Promise((resolve) => {
-    console.log('setting token to', token)
-    resolve()
+    localStorage.setItem(TOKEN_ITEM_KEY, JSON.stringify(token))
+    axios.defaults.headers.common['Authorization'] = token
+    resolve(token)
   })
 }
 
 export function clearAuthToken() {
   return new Promise((resolve) => {
-    console.log('clearing auth token')
+    localStorage.removeItem(TOKEN_ITEM_KEY)
+    axios.defaults.headers.common['Authorization'] = null
+
     resolve()
   })
+}
+
+export function getAuthTokenFromStorage() {
+  return Maybe.fromNullable(localStorage.getItem(TOKEN_ITEM_KEY))
 }
